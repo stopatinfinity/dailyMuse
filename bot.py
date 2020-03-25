5f6bd01d73a9d2da5f9bfbb6799763a8b679c8aa
Nevin Choi <nevinchoi@Nevins-MBP.lan>
2020-03-24 19:13:03 -0700

    Create repository.

---
+++ bot.py
@@ -0,0 +1,14 @@
+import tweepy
+
+print('Hello World!')
+
+
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth) # this is our twitter object
+
+mentions = api.mentions_timeline()
+
+for mention in mentions:
+        print(str(mention.id) + ' - ' + mention.text)
