377f1b5a21cddaa7aa5778ef4e1e6e8a2ac1fdbf
stopatinfinity <nevinchoi@gmail.com>
2020-04-24 22:28:07 -0700

    Remodeled api class.

--- bot.py
+++ bot.py
@@ -1,22 +1,10 @@
 import tweepy
+import re
 import time
 import random
 
 from keys import *
 from youtube import *
-
-print('Welcome to Daily Muse!', flush = True)
-
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
-auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
-api = tweepy.API(auth) # this is our twitter object
-
-"""
-mentions = api.mentions_timeline()
-for mention in mentions:
-    print(str(mention.id) + ' - ' + mention.text)
-"""
-
 
 FILE_NAME = 'last_seen_id.txt'
 
@@ -32,7 +20,73 @@ def store_last_seen_id(last_seen_id, file_name):
     f_write.close()
     return
 
+class twitterClient(object):
+    def __init__(self):
+        try:
+            self.auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+            self.auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+            self.api = tweepy.API(auth) # this is our twitter object
+            print('Welcome to Daily Muse!', flush = True)
+        except:
+            print("Error: Authentication Failed.")
+    
+    def clean_tweet(self, tweet):
+        return ' '.join(re.sub("(@[A-Za-z0-9]+)|([^0-9A-Za-z \t]) | (\w+:\/\/\S+)", " ", tweet).split())
+
+    def get_tweet_sentiment(self, tweet): 
+        ''' 
+        Utility function to classify sentiment of passed tweet 
+        using textblob's sentiment method 
+        '''
+        # create TextBlob object of passed tweet text 
+        analysis = TextBlob(self.clean_tweet(tweet)) 
+        # set sentiment 
+        if analysis.sentiment.polarity > 0: 
+            return 'positive'
+        elif analysis.sentiment.polarity == 0: 
+            return 'neutral'
+        else: 
+            return 'negative'
+
+    def get_tweets(self, query, count = 10): 
+        ''' 
+        Main function to fetch tweets and parse them. 
+        '''
+        # empty list to store parsed tweets 
+        tweets = [] 
+  
+        try: 
+            # call twitter api to fetch tweets 
+            fetched_tweets = self.api.search(q = query, count = count) 
+  
+            # parsing tweets one by one 
+            for tweet in fetched_tweets: 
+                # empty dictionary to store required params of a tweet 
+                parsed_tweet = {} 
+  
+                # saving text of tweet 
+                parsed_tweet['text'] = tweet.text 
+                # saving sentiment of tweet 
+                parsed_tweet['sentiment'] = self.get_tweet_sentiment(tweet.text) 
+  
+                # appending parsed tweet to tweets list 
+                if tweet.retweet_count > 0: 
+                    # if tweet has retweets, ensure that it is appended only once 
+                    if parsed_tweet not in tweets: 
+                        tweets.append(parsed_tweet) 
+                else: 
+                    tweets.append(parsed_tweet) 
+  
+            # return parsed tweets 
+            return tweets 
+  
+        except tweepy.TweepError as e: 
+            # print error (if any) 
+            print("Error : " + str(e)) 
+
 def reply_to_tweets():
+    api = twitterClient()
+
     print('Retrieving and replying to tweets...', flush=True)
     
     last_seen_id = retrieve_last_seen_id(FILE_NAME)
